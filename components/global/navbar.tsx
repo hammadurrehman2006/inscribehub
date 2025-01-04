c7bf3046dc53a2ef97e22dbc0caf74fc6b0bfc57
m_hammad-ur-rehman <164639583+hammadurrehman2006@users.noreply.github.com>
2025-01-04 14:49:13 +0500

    Update navbar.tsx

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -15,55 +15,42 @@ async function NavBar() {
                         <Image src="/iblogo.jpg" alt="Logo" width={100} height={100} className="rounded-full" />
                     </Link>
                     <div className="flex items-center gap-14">
-      <Link
-        href="/blog/create"
-        onClick={() => {
-          if (!session?.user) {
-            toast({
-              title: "Please log in to create a blog!",
-              variant: "destructive",
-            });
-          }
-        }}
-      >
-        <span className="max-sm:hidden">Create</span>
-        <BadgePlus className="sm:hidden size-6" />
-      </Link>
-      {session && session?.user ? (
-        <>
-          <form
-            action={async () => {
-              "use server";
-              await signOut({ redirectTo: "/" });
-            }}
-          >
-            <button type="submit">
-              <span className="max-sm:hidden">Logout</span>
-              <LogOut className="sm:hidden text-enerBorn size-6" />
-            </button>
-          </form>
-          <Link href={`/user/${session?.id}`}>
-            <Avatar className="size-10">
-              <AvatarImage
-                src={session?.user?.image || ""}
-                alt={session?.user?.name || ""}
-              />
-              <AvatarFallback>
-                {session?.user?.name?.charAt(0)}
-              </AvatarFallback>
-            </Avatar>
-          </Link>
-        </>
-      ) : (
-        <form
-          action={async () => {
-            "use server";
-            await signIn("github");
-          }}
-        >
-          <button type="submit">LogIn</button>
-        </form>
-      )}
+                                <Link href="/blog/create" onClick={()=>{
+                                    if(!session?.user){
+                                        toast({title:"Please log in to create a blog!"});
+                                    }
+                                }}>
+                                    <span className="max-sm:hidden">Create</span>
+                                    <BadgePlus className="sm:hidden size-6"/>
+
+                                </Link>
+                        {session && session?.user ? (
+                            <>
+                                <form action={async()=>{
+                                    "use server"
+                                    await signOut({redirectTo: "/"})}}>
+                                    <button type="submit"><span className="max-sm:hidden">Logout</span>
+                                    <LogOut className="sm:hidden text-enerBorn size-6"/>
+                                    </button>
+                                </form>
+                                <Link href={`/user/${session?.id}`}>
+                                    <Avatar className="size-10">
+                                        <AvatarImage src={session?.user?.image||""} alt={session?.user?.name||""}/>
+                                        <AvatarFallback>
+                                            {session?.user?.name?.charAt(0)}
+                                        </AvatarFallback>
+                                    </Avatar>
+                                </Link>
+                            </>
+                        ) : (
+                            <form action={async () =>{
+                                "use server"
+                                await signIn("github")}}>
+                                <button type="submit">
+                                    LogIn
+                                </button>
+                            </form>
+                        )}
                     </div>
                 </nav>
             </header>
