2e1bd6d5d63352d0eddb8f59ef9f61aaf1128a1b
m_hammad-ur-rehman <164639583+hammadurrehman2006@users.noreply.github.com>
2025-01-04 14:13:39 +0500

    Update validation.ts

--- lib/validation.ts
+++ lib/validation.ts
@@ -5,13 +5,14 @@ export const formSchema = z.object({
   description: z.string().min(20).max(500),
   category: z.string().min(3).max(25),
   link: z.string().url().refine(async(url) =>{ 
-try {
-    const res = await fetch(url,{method:"HEAD"});
-    const contentType = res.headers.get("content-type");
-    return contentType?.endsWith(".jpg") || contentType?.endsWith(".png") || contentType?.endsWith(".jpeg") || contentType?.endsWith(".webp") || contentType?.endsWith(".gif") || contentType?.endsWith(".svg") || contentType?.startsWith("image/")
-} catch{
-    return false;
-}
+    try {
+      const res = await fetch(url, { method: "HEAD" });
+      const contentType = res.headers.get("content-type") || "";
+      const isImage = contentType.match(/^image\/|\.jpg|\.png|\.jpeg|\.webp|\.gif|\.svg|\.bmp|\.tiff|\.ico|\.cur$/i);
+      return Boolean(isImage);
+  } catch {
+      return false;
+  }
 }),
 blog: z.string().min(10)
 });
